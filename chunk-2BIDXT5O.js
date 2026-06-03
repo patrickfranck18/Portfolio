08d6b7f74251254ed538ded664e01f5af95c97f1
Franck Patrick <franckpatrick18@gmail.com>
2026-06-03 18:17:34 +0200

    Auto-generated commit

--- chunk-2BIDXT5O.js
+++
@@ -1,7 +0,0 @@
-import {
-  startFocusVisible
-} from "./chunk-LPMBLWVS.js";
-import "./chunk-MN6HTTDJ.js";
-export {
-  startFocusVisible
-};
--- chunk-YIHOUFYZ.js
+++
@@ -1,7 +0,0 @@
-import {
-  startFocusVisible
-} from "./chunk-PYCLCT4W.js";
-import "./chunk-MN6HTTDJ.js";
-export {
-  startFocusVisible
-};
